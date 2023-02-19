99e59d9150637947f3e096a278ed4da3c2b6bc34
501A <art71903design@gmail.com>
2023-02-18 17:26:58 -0800

    Deleted onboarding screen component

--- App.js
+++ App.js
@@ -1,39 +1,14 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { OnboardingScreen } from './components/OnboardingScreen';
 import { ProfileScreen } from './components/ProfileScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [firstLaunch, setFirstLaunch] = React.useState(null);
-  React.useEffect(() => {
-    async function setData() {
-      const appData = await AsyncStorage.getItem("appLaunched");
-      if (appData == null) {
-        setFirstLaunch(true);
-        AsyncStorage.setItem("appLaunched", "false");
-      } else {
-        setFirstLaunch(false);
-      }
-    }
-    setData();
-  }, []);
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {firstLaunch && (
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Onboarding"
-            component={OnboardingScreen}
-          />
-        )}
-        {/* <Stack.Screen name="Welcome" component={HomeScreen} /> */}
         <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
