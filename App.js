ce0fbd3f175c39f870400cc14a616c5d789feca7
501A <art71903design@gmail.com>
2023-02-18 23:30:11 -0800

    Added onboarding screen

--- App.js
+++ App.js
@@ -21,83 +21,106 @@ const Tab = createBottomTabNavigator();
 
 
 function App() {
+  const [starting, setStarting] = React.useState(true);
+
   const [userId, setUserId] = React.useState('');
-  
+
   const [userInput, setUserInput] = React.useState('');
   const [passwordInput, setPasswordInput] = React.useState('');
 
   return (
     <>
-      {
-        !userId ?
+      {starting ?
         <View
           style={{
+            padding:24,
             flex:1,
-            alignItems:'center',
-            justifyContent:'center',
+            justifyContent:'space-between'
           }}
         >
-          <Container>
+          <Text style={styles.heading1}>
+            Welcome to{"\n"}
+            Game Guardian
+          </Text>
+          <PrimaryButton            
+            type={"primary"}
+            onPress={()=>setStarting(false)}
+            title={"Next"}
+          />
+        </View>:
+        <>
+          {
+            !userId ?
             <View
               style={{
-                marginBottom:15,
-                minWidth:300
+                flex:1,
+                alignItems:'center',
+                justifyContent:'center',
               }}
             >
-              <Text style={styles.heading1}>
-                Welcome
-              </Text>
-              <Text>
-                Insert your user ID.
-              </Text>
-            </View>
-            <Input
-              placeholder={"Input user ID"}
-              onChange={setUserInput}
-              value={userInput}
-            />
-            <Input
-              placeholder={"Password"}
-              onChange={setPasswordInput}
-              value={passwordInput}
-            />
-            <PrimaryButton
-              type={'filled'}
-              title={'Get Started'}
-              onPress={()=>setUserId(userInput)}
-            />
-          </Container>
-        </View>:
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen
-              name="Profile"
-              component={ProfileScreen}
-              options={{
-                tabBarIcon:({color})=>(
-                  <Icon
-                    name="user"
-                    size={25}
-                    color={color}
-                  />
-                )
-              }}
-            />
-            <Tab.Screen
-              name="How To"
-              component={HowToScreen}
-              options={{
-                tabBarIcon:({color})=>(
-                  <Icon
-                    name="file"
-                    size={25}
-                    color={color}
-                  />
-                )
-              }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
+              <Container>
+                <View
+                  style={{
+                    marginBottom:15,
+                    minWidth:300
+                  }}
+                >
+                  <Text style={styles.heading1}>
+                    Welcome
+                  </Text>
+                  <Text>
+                    Insert your user ID.
+                  </Text>
+                </View>
+                <Input
+                  placeholder={"Input user ID"}
+                  onChange={setUserInput}
+                  value={userInput}
+                />
+                <Input
+                  placeholder={"Password"}
+                  onChange={setPasswordInput}
+                  value={passwordInput}
+                />
+                <PrimaryButton
+                  type={'filled'}
+                  title={'Get Started'}
+                  onPress={()=>setUserId(userInput)}
+                />
+              </Container>
+            </View>:
+            <NavigationContainer>
+              <Tab.Navigator>
+                <Tab.Screen
+                  name="Profile"
+                  component={ProfileScreen}
+                  options={{
+                    tabBarIcon:({color})=>(
+                      <Icon
+                        name="user"
+                        size={25}
+                        color={color}
+                      />
+                    )
+                  }}
+                />
+                <Tab.Screen
+                  name="How To"
+                  component={HowToScreen}
+                  options={{
+                    tabBarIcon:({color})=>(
+                      <Icon
+                        name="file"
+                        size={25}
+                        color={color}
+                      />
+                    )
+                  }}
+                />
+              </Tab.Navigator>
+            </NavigationContainer>
+          }
+        </>
       }
     </>
   );
